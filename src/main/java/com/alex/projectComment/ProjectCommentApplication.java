9d9f79090a5f196f57ff6ebfcf5249cb01e3cd60
Alex <alex2003simao1@gmail.com>
2025-05-27 20:42:25 -0300

    Iniciando e configurando projeto.

---
+++ src/main/java/com/alex/projectComment/ProjectCommentApplication.java
@@ -0,0 +1,13 @@
+package com.alex.projectComment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCommentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectCommentApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/alex/projectComment/ProjectCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.projectComment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
