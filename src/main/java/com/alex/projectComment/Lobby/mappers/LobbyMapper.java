774abcf3a5687fd6dea934155153f0903305dd0a
Alex <alex2003simao1@gmail.com>
2025-06-07 01:25:43 -0300

    fix: Atualizando mapeadores para usar UserMapper. Integrando UserMapper nas classes LobbyMapper e SectionMapper para melhorar a conversão de entidades.

--- src/main/java/com/alex/projectComment/Lobby/mappers/LobbyMapper.java
+++ src/main/java/com/alex/projectComment/Lobby/mappers/LobbyMapper.java
@@ -2,9 +2,10 @@ package com.alex.projectComment.Lobby.mappers;
 
 import com.alex.projectComment.Lobby.dtos.LobbyDTO;
 import com.alex.projectComment.Lobby.entities.Lobby;
+import com.alex.projectComment.User.mappers.UserMapper;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface LobbyMapper {
 
   LobbyDTO toDTO(Lobby entity);
--- src/main/java/com/alex/projectComment/Section/mapper/SectionMapper.java
+++ src/main/java/com/alex/projectComment/Section/mapper/SectionMapper.java
@@ -2,9 +2,10 @@ package com.alex.projectComment.Section.mapper;
 
 import com.alex.projectComment.Section.dtos.SectionDTO;
 import com.alex.projectComment.Section.entities.Section;
+import com.alex.projectComment.User.mappers.UserMapper;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface SectionMapper {
   SectionDTO toDTO(Section section);
 
